58587513e1b88a78b929bff5c7ae954e5ac46aed
ZiadAhmedH <ziad.ahmedcs21@fcai.usc.edu.eg>
2024-07-15 18:32:10 +0300

    solve Beecrowd basic

--- java/ExtremelyBasic.java
+++ java/ExtremelyBasic.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class ExtremelyBasic {
-    
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int X1 = in.nextInt();
+        int X2 = in.nextInt();
+        System.out.println("X = "+(X1+X2));
         
     }
 }
